b9da1bbc6d7cac4561bfb706843cf83690afc0a2
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-05-04 08:20:37 +0300

    Add Procfile

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,23 @@
 from flask import render_template
-from main import main
+from  .import main
+from ..requests get_sources, get_articles
 
 
 @main.route('/')
 def index():
     """
+    Function that return news souces
 
     """
-    return render_template
+    sources = get_sources()
+    title = 'News Now'
+    return render_template('index.html', sources = sources, title = title)
+
+@main.route('/articles/<sources_id>')
+def articles(sources_id):
+    '''
+    Returns the articles for the given sources
+    '''
+    articles = get_articles(sources_id)
+    
+    return render_template('articles.html',articles = articles)
--- manage.py
+++ manage.py
@@ -4,5 +4,18 @@ from flask_script import Manager,Server
 
 app = create_app('development')
 
+manager = Manager(app)
+manager.add_command('server', Server)
+
+@manager.command
+def test():
+    '''
+    Run the unittest
+    '''
+    import unnittest
+    test = unittest.Testloader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
 if __name__ == '__main__':
     app.run()
