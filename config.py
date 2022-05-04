3ec600df4fbae13605bf87256ad5ac2d5d0be009
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-05-04 07:51:19 +0300

    Update config.py file

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ class Config:
     pass
 
     
-    # NEWS_API_SOURCES_URL = ''
-    # NEWS_API_ARTICLES_URL = ''
-    # NEWS_API_KEY = 'b3dbb2b9093b4fb4bf50993d2e0ab203'
-    # SECRET_KEY=''
+    NEWS_API_SOURCES_URL = 'https://newsapi.org/v2/sources?apiKey={}'
+    NEWS_API_ARTICLES_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+    NEWS_API_KEY = 'b3dbb2b9093b4fb4bf50993d2e0ab203'
+    SECRET_KEY='SECRET_KEY'
 
 
 class ProdConfig(Config):
